22adea05f07e93ff86432a297e97e734a1aaf7a5
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-09-28 22:56:38 +0200

    Add code for Timer

--- app/script.js
+++ app/script.js
@@ -1,37 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-  const [status, setStatus] = useState("off");
-  // The "status" variable informs about the application status: off, work or rest. By default we set it to "off".
+  const [status, setStatus] = useState('off');
   const [time, setTime] = useState(null);
-  // The "time" variable will store the counter time in seconds. Initially, the value is set to null.
   const [timer, setTimer] = useState(null);
-  // The "timer" variable will store the future interval (interval). First we set it to null.
+
+  let seconds = String(time % 60).padStart(2, '0');
+  let minutes = String(Math.floor(time / 60)).padStart(2, '0');
+
+  const startTimer = () => {
+    setTime(1200);
+    setStatus('work');
+    setTimer(
+      setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000)
+    );
+  };
+
+  const stopTimer = () => {
+    setTimer(null);
+    setTime(null);
+    setStatus('off');
+    clearInterval(timer);
+  };
+
+  const closeApp = () => {
+    window.close();
+  };
+
+  const playBell = () => {
+    const bell = new Audio('./sounds/bell.wav');
+    bell.play();
+  };
+
+  useEffect(() => {
+    if (time === 0) {
+      playBell();
+      if (status === 'work') {
+        setStatus('rest');
+        setTime(20);
+      } else {
+        setStatus('work');
+        setTime(1200);
+      }
+    }
+  }, [time]);
+
   return (
     <div>
       <h1>Protect your eyes</h1>
-      {status === "off" && (
+      {status === 'off' && (
         <div>
-          <p>
-            According to optometrists in order to save your eyes, you should
-            follow the 20/20/20. It means you should to rest your eyes every 20
-            minutes for 20 seconds by looking more than 20 feet away.
-          </p>
-          <p>
-            This app will help you track your time and inform you when it's time
-            to rest.
-          </p>
+          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+          <p>This app will help you track your time and inform you when its time to rest.</p>
         </div>
       )}
-      {status === "work" && <img src="./images/work.png" />}
-      {status === "rest" && <img src="./images/rest.png" />}
-      {status !== "off" && <div className="timer">18:23</div>}
-      {status === "off" && <button className="btn">Start</button>}
-      {status !== "off" && <button className="btn">Stop</button>}
-      <button className="btn btn-close">X</button>
+      {status === 'work' && <img src='./images/work.png' />}
+      {status === 'rest' && <img src='./images/rest.png' />}
+      {status !== 'off' && (
+        <div className='timer'>
+          {minutes}:{seconds}
+        </div>
+      )}
+      {status === 'off' && (
+        <button className='btn' onClick={startTimer}>
+          Start
+        </button>
+      )}
+      {status !== 'off' && (
+        <button className='btn' onClick={stopTimer}>
+          Stop
+        </button>
+      )}
+      <button className='btn btn-close' onClick={closeApp}>
+        X
+      </button>
     </div>
-  )
+  );
 };
 
 render(<App />, document.querySelector('#app'));
